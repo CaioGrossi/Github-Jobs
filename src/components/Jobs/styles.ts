50671a1ebbe72bedc5eefc96e25599173e1cd624
CaioGrossi <caiogrossi6@gmail.com>
2021-03-22 12:25:59 -0300

    Added display flex to Jobs component

--- src/components/Jobs/styles.ts
+++ src/components/Jobs/styles.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import * as CardJobStyles from '../CardJob/styles';
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   grid-area: jobs;
   ${CardJobStyles.Wrapper} {
     margin-bottom: 3.2rem;
