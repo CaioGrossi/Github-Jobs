0747e569871efe8d52c525198ef437f55da466cc
Caio Grossi <59300299+CaioGrossi@users.noreply.github.com>
2021-05-13 22:04:44 -0300

    Update index.tsx

--- src/pages/JobDetail/index.tsx
+++ src/pages/JobDetail/index.tsx
@@ -58,7 +58,7 @@ const JobDetail = () => {
 
         <S.HowApply>
           <h4>HOW TO APPLY</h4>
-          <div dangerouslySetInnerHTML={{ __html: job.how_to_apply }}></div>
+          <div style={{wordBreak: "break-word"}} dangerouslySetInnerHTML={{ __html: job.how_to_apply }}></div>
         </S.HowApply>
 
         <S.MainContent>
